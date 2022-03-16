7a188ba1d9f7a2e8482820b69e577bec6592aef4
Arebiter <pasandharmasena@gmail.com>
2022-03-16 19:00:02 -0400

    Add basic image elements to show armor pieces being chosen

--- src/Armor_images.js
+++ src/Armor_images.js
@@ -3,6 +3,18 @@ import { useState, useEffect } from "react";
 import "../src/CSS_folder/Armor_set_list.scss";
 
 
-export const ArmorImages = ({ armorSets }) => {
+export const ArmorImages = ({ armorPieces }) => {
+    console.log(armorPieces);
 
+    const { head, chest, gloves, waist, legs } = armorPieces;
+
+    return (
+        <div>
+            <img src={head !== "" ? (head.assets.imageMale) : null} />
+            <img src={chest !== "" ? (chest.assets.imageMale) : null} />
+            <img src={gloves !== "" ? (gloves.assets.imageMale) : null} />
+            <img src={waist !== "" ? (waist.assets.imageMale) : null} />
+            <img src={legs !== "" ? (legs.assets.imageMale) : null} />
+        </div>
+    )
 }
