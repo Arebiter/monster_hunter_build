d196df67f13a8a9118ed168c952f104bbbfb0919
Arebiter <pasandharmasena@gmail.com>
2022-03-17 02:36:53 -0400

    Add areas for armor image pieces, grouped chest and gloves

--- src/Armor_images.js
+++ src/Armor_images.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import "../src/CSS_folder/Armor_set_list.scss";
+import "../src/CSS_folder/Armor_images.scss";
 
 
 export const ArmorImages = ({ armorPieces }) => {
@@ -9,12 +9,39 @@ export const ArmorImages = ({ armorPieces }) => {
     const { head, chest, gloves, waist, legs } = armorPieces;
 
     return (
-        <div>
-            <img src={head !== "" ? (head.assets.imageMale) : null} />
-            <img src={chest !== "" ? (chest.assets.imageMale) : null} />
-            <img src={gloves !== "" ? (gloves.assets.imageMale) : null} />
-            <img src={waist !== "" ? (waist.assets.imageMale) : null} />
-            <img src={legs !== "" ? (legs.assets.imageMale) : null} />
-        </div>
+        <div className="armor-images">
+            <div className="armor-piece-group">
+                <div>Head</div>
+                <div className="armor-piece-div">
+                    <img src={head !== "" ? (head.assets.imageMale) : null} />
+                </div>
+            </div>
+            <div className="armor-chest-gloves-div">
+                <div className="armor-piece-group">
+                    <div>Chest</div>
+                    <div className="armor-piece-div">
+                        <img src={chest !== "" ? (chest.assets.imageMale) : null} />
+                    </div>
+                </div>
+                <div className="armor-piece-group">
+                    <div>Gloves</div>
+                    <div className="armor-piece-div">
+                        <img src={gloves !== "" ? (gloves.assets.imageMale) : null} />
+                    </div>
+                </div>
+            </div>
+            <div className="armor-piece-group">
+                <div>Waist</div>
+                <div className="armor-piece-div">
+                    <img src={waist !== "" ? (waist.assets.imageMale) : null} />
+                </div>
+            </div>
+            <div className="armor-piece-group">
+                <div>Legs</div>
+                <div className="armor-piece-div">
+                    <img src={legs !== "" ? (legs.assets.imageMale) : null} />
+                </div>
+            </div>
+        </div >
     )
 }
