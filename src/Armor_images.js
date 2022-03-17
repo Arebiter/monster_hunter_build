394d0a80d21465912a511e55e010ceb2df279812
Arebiter <pasandharmasena@gmail.com>
2022-03-17 19:11:58 -0400

    Change ternary for displaying images to account for new default value for props

--- src/Armor_images.js
+++ src/Armor_images.js
@@ -4,7 +4,7 @@ import "../src/CSS_folder/Armor_images.scss";
 
 
 export const ArmorImages = ({ armorPieces }) => {
-    console.log(armorPieces);
+
 
     const { head, chest, gloves, waist, legs } = armorPieces;
 
@@ -13,33 +13,33 @@ export const ArmorImages = ({ armorPieces }) => {
             <div className="armor-piece-group">
                 <div>Head</div>
                 <div className="armor-piece-div">
-                    <img src={head !== "" ? (head.assets.imageMale) : null} />
+                    <img src={head ? (head.assets.imageMale) : null} />
                 </div>
             </div>
             <div className="armor-chest-gloves-div">
                 <div className="armor-piece-group">
                     <div>Chest</div>
                     <div className="armor-piece-div">
-                        <img src={chest !== "" ? (chest.assets.imageMale) : null} />
+                        <img src={chest ? (chest.assets.imageMale) : null} />
                     </div>
                 </div>
                 <div className="armor-piece-group">
                     <div>Gloves</div>
                     <div className="armor-piece-div">
-                        <img src={gloves !== "" ? (gloves.assets.imageMale) : null} />
+                        <img src={gloves ? (gloves.assets.imageMale) : null} />
                     </div>
                 </div>
             </div>
             <div className="armor-piece-group">
                 <div>Waist</div>
                 <div className="armor-piece-div">
-                    <img src={waist !== "" ? (waist.assets.imageMale) : null} />
+                    <img src={waist ? (waist.assets.imageMale) : null} />
                 </div>
             </div>
             <div className="armor-piece-group">
                 <div>Legs</div>
                 <div className="armor-piece-div">
-                    <img src={legs !== "" ? (legs.assets.imageMale) : null} />
+                    <img src={legs ? (legs.assets.imageMale) : null} />
                 </div>
             </div>
         </div >
