1185ec69dc426efbff7e07a26b4ec7f24c6743a7
Arebiter <pasandharmasena@gmail.com>
2022-03-17 23:05:41 -0400

    Add armor stats section class name to organize content

--- src/components/Armor_stats.js
+++ src/components/Armor_stats.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import "../CSS_folder/Armor_images.scss";
+import "../CSS_folder/Armor_stats.scss";
 import { CurrentArmorStats } from './Current_armor_stats';
 import { TotalArmorStats } from './Total_armor_stats';
 
@@ -8,7 +8,7 @@ import { TotalArmorStats } from './Total_armor_stats';
 export const ArmorStats = ({ currentArmorPiece, armorPieces }) => {
 
     return (
-        <div>
+        <div className="armor-stats-section">
             <CurrentArmorStats currentArmorPiece={currentArmorPiece} />
             <TotalArmorStats armorPieces={armorPieces} />
         </div>
