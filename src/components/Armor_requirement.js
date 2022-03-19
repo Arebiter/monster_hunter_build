0fc552aa558ce2ce5094172ebfe43ceb6588add7
Arebiter <pasandharmasena@gmail.com>
2022-03-18 23:48:13 -0400

    Fix useEffect issue for armor requirements, was using useEffect instead of useState

--- src/components/Armor_requirement.js
+++ src/components/Armor_requirement.js
@@ -3,76 +3,31 @@ import { useState, useEffect } from "react";
 import "../CSS_folder/Armor_requirements.scss";
 
 export const ArmorRequirements = ({ currentArmorPiece }) => {
-    const [armorPic, setArmorPic] = useEffect({
-        "image": ""
+    const [armorPic, setArmorPic] = useState({
+        "image": null
     });
-    console.log(armorPic)
-    const [armorMaterials, setArmorMaterials] = useEffect({
+    const [armorMaterials, setArmorMaterials] = useState({
         "materials": []
     });
-    const [armorName, setArmorName] = useEffect({
+    const [armorName, setArmorName] = useState({
         "name": "Armor"
     });
-    const [totalCost, setTotalCost] = useEffect(0);
 
-    const { armor } = currentArmorPiece;
-    let cost = 0
+    let totalCost = 0;
+    const materialArr = currentArmorPiece.armor === "" ? ([]) : (currentArmorPiece.armor.crafting.materials);
+    const imagesArr = currentArmorPiece.armor === "" ? (null) : (currentArmorPiece.armor.assets.imageMale);
+    const nameState = currentArmorPiece.armor === "" ? ("Armor") : (currentArmorPiece.armor.name);
+
     useEffect(() => {
-        setArmorPic(prevState => ({ ...prevState, image: armor.assets.imageMale }));
-        setArmorMaterials(prevState => ({ ...prevState, materials: armor.crafting.materials }));
-        setArmorName(prevState => ({ ...prevState, name: armor.name }));
-    }, [armor])
+        setArmorPic(prevState => ({ ...prevState, image: imagesArr }));
+        setArmorMaterials(prevState => ({ ...prevState, materials: materialArr }));
+        setArmorName(prevState => ({ ...prevState, name: nameState }));
+    }, [currentArmorPiece.armor])
 
-
-    if (currentArmorPiece.armor === "") {
-        return (
-            <div className="armor-requirement-section">
-                <div className="requirement-border-top">
-                    <img className="requirement-border-top-left-corner" src={require(`../icons/alt_top_left_corner.png`)} />
-                    <img className="requirement-border-top-left" src={require(`../icons/top_line.png`)} />
-                    <img className="requirement-border-top-center" src={require(`../icons/top_center.png`)} />
-                    <img className="requirement-border-top-right" src={require(`../icons/top_line.png`)} />
-                    <img className="requirement-border-top-right-corner" src={require(`../icons/alt_top_right_corner.png`)} />
-                </div>
-                <div className="armor-piece-group down">
-                    <div>Armor</div>
-                    <div className="armor-piece-div">
-                    </div>
-                </div>
-                <div className="requirement-border-mid">
-                    <img className="requirement-border-top-left-corner" src={require(`../icons/top_left_corner.png`)} />
-                    <img className="requirement-border-top-line" src={require(`../icons/top_line.png`)} />
-                    <img className="requirement-border-top-right-corner" src={require(`../icons/top_right_corner.png`)} />
-                </div>
-                <div className="armor-material-group">
-                    <div className="title">Required Materials</div>
-                    <div className="armor-material-section">
-                    </div>
-                </div>
-                <div className="requirement-border-bot">
-                    <img className="requirement-border-top-left-corner" src={require(`../icons/top_left_corner.png`)} />
-                    <img className="requirement-border-top-line" src={require(`../icons/top_line.png`)} />
-                    <img className="requirement-border-top-right-corner" src={require(`../icons/top_right_corner.png`)} />
-                </div>
-                <div className="armor-cost">
-                    <div className="title">Required Cost</div>
-                    <div className="armor-money">
-                        <img className="coin" src={require(`../icons/money.png`)} />
-                        <div>0 z</div>
-                    </div>
-                </div>
-                <div className="requirement-border-under">
-                    <img className="requirement-border-top-left-corner" src={require(`../icons/bottom_left_corner.png`)} />
-                    <img className="requirement-border-top-line" src={require(`../icons/top_line.png`)} />
-                    <img className="requirement-border-top-right-corner" src={require(`../icons/bottom_right_corner.png`)} />
-                </div>
-            </div >
-        )
-    }
     const materials = (
-        armorMaterials.length === 0 ? (null) : (
+        armorMaterials.materials === undefined ? (null) : (
             armorMaterials.materials.map((material, idx) => {
-                cost += (material.quantity * material.item.value)
+                totalCost += (material.quantity * material.item.value)
                 return (
                     <div key={`${idx}-${material.item.name}-${armorName}`} className="armor-material">
                         <div className="armor-material-name">{material.item.name}</div>
@@ -93,11 +48,9 @@ export const ArmorRequirements = ({ currentArmorPiece }) => {
                 <img className="requirement-border-top-right-corner" src={require(`../icons/alt_top_right_corner.png`)} />
             </div>
             <div className="armor-piece-group down">
-                <div>{armorName}</div>
+                <div>{armorName.name}</div>
                 <div className="armor-piece-div">
-                    {armorPic.image === "" ? (null) : (
-                        <img src={armorPic.image} />
-                    )}
+                    <img src={armorPic.image} />
                 </div>
             </div>
             <div className="requirement-border-mid">
