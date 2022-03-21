870767c66a16a6991a23797c4771e493f60a26ba
Arebiter <pasandharmasena@gmail.com>
2022-03-20 20:49:07 -0400

    Change armor piece title to show armor piece name when armor is selected

--- src/components/Armor_images.js
+++ src/components/Armor_images.js
@@ -11,33 +11,43 @@ export const ArmorImages = ({ armorPieces }) => {
     return (
         <div className="armor-images-section">
             <div className="armor-piece-group">
-                <div>Head</div>
+                <div className="armor-piece-name">
+                    {head ? (head.name) : "Head"}
+                </div>
                 <div className="armor-piece-div">
                     <img src={head ? (head.assets.imageMale) : null} />
                 </div>
             </div>
             <div className="armor-chest-gloves-div">
                 <div className="armor-piece-group">
-                    <div>Chest</div>
+                    <div className="armor-piece-name">
+                        {chest ? (chest.name) : "Chest"}
+                    </div>
                     <div className="armor-piece-div">
                         <img src={chest ? (chest.assets.imageMale) : null} />
                     </div>
                 </div>
                 <div className="armor-piece-group">
-                    <div>Gloves</div>
+                    <div className="armor-piece-name">
+                        {gloves ? (gloves.name) : "Gloves"}
+                    </div>
                     <div className="armor-piece-div">
                         <img src={gloves ? (gloves.assets.imageMale) : null} />
                     </div>
                 </div>
             </div>
             <div className="armor-piece-group">
-                <div>Waist</div>
+                <div className="armor-piece-name">
+                    {waist ? (waist.name) : "Waist"}
+                </div>
                 <div className="armor-piece-div">
                     <img src={waist ? (waist.assets.imageMale) : null} />
                 </div>
             </div>
             <div className="armor-piece-group">
-                <div>Legs</div>
+                <div className="armor-piece-name">
+                    {legs ? (legs.name) : "Legs"}
+                </div>
                 <div className="armor-piece-div">
                     <img src={legs ? (legs.assets.imageMale) : null} />
                 </div>
